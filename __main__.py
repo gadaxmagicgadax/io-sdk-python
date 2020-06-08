a1165c3220fd6a0ba11cc75260541f48e143b63e
Pierluigi Di Lorenzo <pierluigi.dilorenzo@gmail.com>
2020-06-08 17:36:34 +0200

    import messages from graphql with python:3 action

--- __main__.py
+++ __main__.py
@@ -11,8 +11,11 @@ import json
 #                                         titolo AS subject FROM messages
 
 def main(args):
+
+	import config
+	
 	query = """query {
-    messages {
+    data : messages {
 		due_date : scadenza
 		fiscal_code : destinatario
 		markdown : testo
@@ -20,12 +23,25 @@ def main(args):
   		}
 	}"""
 
-	url = args.get("url") if args.get("url") else 'http://172.17.0.1:8080/v1/graphql'
- 	
-	r = requests.post(url, json={'query': query})
-	print(r.status_code)
-	print(r.text)
-	print(type(r.text))
+	# url = args.get("url") if args.get("url") else 'http://172.17.0.1:8080/v1/graphql'
+	if args.get("url") == None:
+		print(config.emptyForm)
+		json_data = json.loads(config.emptyForm)
+		print(json_data)
+		return {"body": json_data}
+
+	try:
+		r = requests.post(args.get("url"), json={'query': query})
+	except requests.exceptions.RequestException as e:
+		print(e)
+		#json_data = json.loads(str(e))
+		#print(json_data)
+		return {"body": str(e)}
+
 	json_data = json.loads(r.text)
+
+	# parse json_data to add fixed fields (should be made by the query?)	
+	for item in json_data['data']['data']:
+    		item.update(amount = 0, invalid_after_due_data = False, notice_number = 1)
 	print(json_data)
-	return {"body": json_data}
+	return {"body": json_data['data']}
--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 cd "$(dirname $0)"
 if ! test -f index.zip 
 then echo "preparing archive, please wait..."
-     zip -qr index.zip __main__.py
+     zip -qr index.zip __main__.py config.py
 fi
 wsk action update iosdk/import index.zip --kind python:3 --web true
  
