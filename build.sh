ef2e6d4a228a278f88b485a7592821d01370807e
Giovanni Gadaleta <61932706+gadaxmagicgadax@users.noreply.github.com>
2020-07-26 15:56:55 +0200

    Update build.sh
    the if statement in the shell denied the preparation of new index.zip in case of changes and redeploy of __main__.py

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd "$(dirname $0)"
-if ! test -f index.zip 
-then echo "preparing archive, please wait..."
-     zip -qr index.zip __main__.py config.py
-fi
+
+echo "preparing archive, please wait..."
+zip -qr index.zip __main__.py config.py
+
 wsk action update iosdk/import index.zip --kind python:3 --web true
  
